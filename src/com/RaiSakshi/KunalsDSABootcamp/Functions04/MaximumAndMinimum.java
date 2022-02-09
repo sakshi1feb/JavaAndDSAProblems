c248a6884c9dbcaff33bec96fe930c5262850f32
Sakshi Rai <sakshi1feb@gmail.com>
2022-02-09 05:39:57 +0530

    A program using methods to find minimum and maximum of three numbers in java

---
+++ src/com/RaiSakshi/KunalsDSABootcamp/Functions04/MaximumAndMinimum.java
@@ -0,0 +1,24 @@
+package com.RaiSakshi.KunalsDSABootcamp.Functions04;
+import java.util.Scanner;
+
+public class MaximumAndMinimum {
+    // A program to define two methods to print the maximum and the minimum number respectively among three
+    // numbers entered by the user.
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter three numbers ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println("The maximum of the three numbers is "+maximum(a,b,c));
+        System.out.println("The minimum of the three numbers is "+minimum(a,b,c));
+    }
+
+     static int  minimum(int a, int b, int c) {
+        return Math.min(a,Math.min(b,c));
+    }
+
+    static int  maximum(int a, int b, int c) {
+        return Math.max(a,Math.max(b,c));
+    }
+}
