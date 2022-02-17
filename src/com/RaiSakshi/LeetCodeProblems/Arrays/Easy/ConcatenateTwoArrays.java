1205e0f0ceac0560c079cc6fdcbfe6834427f6fb
Sakshi Rai <sakshi1feb@gmail.com>
2022-02-17 22:12:37 +0530

    Leet code Problem to concatenate two arrays

---
+++ src/com/RaiSakshi/LeetCodeProblems/Arrays/Easy/ConcatenateTwoArrays.java
@@ -0,0 +1,30 @@
+package com.RaiSakshi.LeetCodeProblems.Arrays.Easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConcatenateTwoArrays { ;
+            public static int[] getConcatenation(int[] nums) {
+            int l=nums.length *2;
+            int[] ans=new int [l];
+            int i=0;
+            for(int k=1;k<=2;k++){
+                for(int j=0;j<nums.length;j++){
+                    ans[i]=nums[j];
+                    i++;
+                }
+            }
+            return ans;
+        }
+        public static void main(String[] args){
+            Scanner sc=new Scanner (System.in);
+            System.out.println("Enter the length of an array");
+            int l =sc.nextInt();
+            System.out.println("Enter the elements of the array");
+            int[] arr=new int[l];
+            for(int i=0;i<l;i++)
+                arr[i]=sc.nextInt();
+            System.out.println(Arrays.toString(getConcatenation(arr)));
+        }
+    }
+
