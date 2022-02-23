f96771f5bc21d740d93769d18a81e6a5c2614f30
Sakshi Rai <sakshi1feb@gmail.com>
2022-02-23 20:10:22 +0530

    A program to swap the elements of array

---
+++ src/com/RaiSakshi/KunalsDSABootcamp/Arrays/SwapElement.java
@@ -0,0 +1,28 @@
+package com.RaiSakshi.KunalsDSABootcamp.Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SwapElement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter length of the array");
+        int l=sc.nextInt();
+        int[] arr= new int[l];
+        System.out.println("Enter the elements of the array ");
+        for(int i=0;i<arr.length;i++)
+            arr[i]=sc.nextInt();
+        System.out.println("Enter the index you want to swap");
+        int index1=sc.nextInt();
+        System.out.println("Enter the index with which you want to swap");
+        int index2=sc.nextInt();
+        System.out.println("The new Swapped array is"+ Arrays.toString(swap(arr, index1, index2)));
+    }
+    static int[] swap(int[] arr,int index1,int index2){
+        int temp;
+        temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+        return arr;
+    }
+}
