98223c5e96b6cd325bb2d6cb2d7c039642b690ec
Sakshi Rai <sakshi1feb@gmail.com>
2022-03-20 10:52:14 +0530

    A simple program to print the pattern2 of kk bootcamp

---
+++ src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern2.java
@@ -0,0 +1,20 @@
+package com.RaiSakshi.KunalsDSABootcamp.Patterns_09;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows in the pattern ");
+        int n=sc.nextInt();
+        pattern(n);
+    }
+
+    static void pattern(int n) {
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
