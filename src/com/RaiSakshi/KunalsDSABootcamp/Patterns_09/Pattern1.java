a4aeef73603adb5a7898979a7f1a8a6c9ab449c0
Sakshi Rai <sakshi1feb@gmail.com>
2022-03-20 11:14:37 +0530

    adding spaces to the *'s

--- src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern1.java
+++ src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern1.java
@@ -18,7 +18,7 @@ public class Pattern1 {
     static void pattern(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=5;j++)
-                System.out.print("*");
+                System.out.print("* ");
             System.out.println();
         }
     }
--- src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern2.java
+++ src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern2.java
@@ -13,7 +13,7 @@ public class Pattern2 {
     static void pattern(int n) {
         for (int i=1;i<=n;i++){
             for(int j=1;j<=i;j++)
-                System.out.print("*");
+                System.out.print("* ");
             System.out.println();
         }
     }
