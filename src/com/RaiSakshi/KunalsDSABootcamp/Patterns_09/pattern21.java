9d2529ec4a06b8bf2a95ba2f002656dc22ecc294
Sakshi Rai <sakshi1feb@gmail.com>
2022-03-23 10:31:30 +0530

    Pattern 21 of kk bootcamp

---
+++ src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/pattern21.java
@@ -0,0 +1,12 @@
+package com.RaiSakshi.KunalsDSABootcamp.Patterns_09;
+
+public class pattern21 {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++)
+                System.out.print(k+++" ");
+            System.out.println();
+        }
+    }
+}
