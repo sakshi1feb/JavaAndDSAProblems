bc836b353420c5edaf0965401fa8f5244068bede
Sakshi Rai <sakshi1feb@gmail.com>
2022-03-23 09:56:12 +0530

    Pattern 30 of kk bootcamp

--- src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern30.java
+++ src/com/RaiSakshi/KunalsDSABootcamp/Patterns_09/Pattern30.java
@@ -6,9 +6,9 @@ public class Pattern30 {
             for(int sp=1;sp<=5-i;sp++)
                 System.out.print("  ");
             for(int j=i;j>=1;j--)
-                System.out.print(j+"  ");
+                System.out.print(j+" ");
             for(int c=2;c<=i;c++)
-                System.out.print(c+"  ");
+                System.out.print(c+" ");
             System.out.println();
         }
     }
